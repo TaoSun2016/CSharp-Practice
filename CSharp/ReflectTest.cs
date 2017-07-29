b676b58996c3a4bffaf8bbf9c40f5f8842fc188c
TaoSun2016 <kevinsun9516@gmail.com>
2017-07-29 21:27:43 +1200

    add class ReflectTest

---
+++ CSharp/ReflectTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class ReflectTest
+    {
+        public void test()
+        {
+
+        }
+        // Display method names of type.
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("***** Methods *****");
+            var methodNames = from n in t.GetMethods()
+                              select n.Name;
+            foreach (var name in methodNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+
+        // Display field names of type.
+        static void ListFields(Type t)
+        {
+            Console.WriteLine("***** Fields *****");
+            var fieldNames = from f in t.GetFields()
+                             select f.Name;
+            foreach (var name in fieldNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+
+        // Display property names of type.
+        static void ListProps(Type t)
+        {
+            Console.WriteLine("***** Properties *****");
+            var propNames = from p in t.GetProperties()
+                            select p.Name;
+            foreach (var name in propNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+
+        // Display implemented interfaces.
+        static void ListInterfaces(Type t)
+        {
+            Console.WriteLine("***** Interfaces *****");
+            var ifaces = from i in t.GetInterfaces()
+                         select i;
+            foreach (Type i in ifaces)
+                Console.WriteLine("->{0}", i.Name);
+        }
+
+        static void ListVariousStats(Type t)
+        {
+            Console.WriteLine("***** Various Statistics * ****");
+            Console.WriteLine("Base class is: {0}", t.BaseType);
+            Console.WriteLine("Is type abstract? {0}", t.IsAbstract);
+            Console.WriteLine("Is type sealed? {0}", t.IsSealed);
+            Console.WriteLine("Is type generic? {0}", t.IsGenericTypeDefinition);
+            Console.WriteLine("Is type a class type? {0}", t.IsClass);
+            Console.WriteLine();
+        }
+    }
+}
