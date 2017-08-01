3866698f79ac43e0c0c50e4775dd55ff173c329e
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-01 21:21:17 +1200

    add new project LateBindingApp

---
+++ LateBindingApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateBindingApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Late Binding * ****");
+            // Try to load a local copy of CarLibrary.
+            Assembly a = null;
+            try
+            {
+                a = Assembly.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (a != null)
+                CreateUsingLateBinding(a);
+            Console.ReadLine();
+        }
+        static void CreateUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Get metadata for the Minivan type.
+                Type miniVan  = asm.GetType("CarLibrary.MiniVan");
+                // Create a Minivan instance on the fly.
+                object obj = Activator.CreateInstance(miniVan);
+                Console.WriteLine("Created a {0} using late binding!", obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
