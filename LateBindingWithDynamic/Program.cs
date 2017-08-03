c1615efe08193ad54c55b62ace9005f3b19ed0aa
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-04 10:44:47 +1200

    add method AddWithReflection

--- LateBindingWithDynamic/Program.cs
+++ LateBindingWithDynamic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace LateBindingWithDynamic
 {
@@ -10,6 +11,26 @@ namespace LateBindingWithDynamic
     {
         static void Main(string[] args)
         {
+        }
+        private static void AddWithReflection()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+            try
+            {
+                // Get metadata for the SimpleMath type.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Create a SimpleMath on the fly.
+                object obj = Activator.CreateInstance(math);
+                // Get info for Add.
+                MethodInfo mi = math.GetMethod("Add");
+                // Invoke method (with parameters).
+                object[] args = { 10, 70 };
+                Console.WriteLine("Result is: {0}", mi.Invoke(obj, args));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
