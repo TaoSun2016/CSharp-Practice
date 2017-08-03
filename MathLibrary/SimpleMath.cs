3c170105b343cfdd70536b26a9dd60df7563f461
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-04 10:39:13 +1200

    rename file name and add method

--- MathLibrary/SimpleMath.cs
+++ MathLibrary/SimpleMath.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MathLibrary
 {
-    public class Class1
+    public class SimpleMath
     {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
