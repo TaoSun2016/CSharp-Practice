7c79d748e15804bf8a03bc471cdf946c6200e689
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-05 11:44:59 +1200

    output infor of appdamin

--- DefaultAppDomainApp/Program.cs
+++ DefaultAppDomainApp/Program.cs
@@ -10,6 +10,19 @@ namespace DefaultAppDomainApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with the default AppDomain*****\n");
+            DisplayDADStats();
+            Console.ReadLine();
+        }
+        private static void DisplayDADStats()
+        {
+            // Get access to the AppDomain for the current thread.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            // Print out various stats about this domain.
+            Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
+            Console.WriteLine("ID of domain in this process: {0} ", defaultAD.Id);
+            Console.WriteLine("Is this the default domain ?: {0} ", defaultAD.IsDefaultAppDomain());
+            Console.WriteLine("Base directory of this domain: {0}", defaultAD.BaseDirectory);
         }
     }
 }
