b2130941592121c4c6ef1b3c5d9ff924ac9b3e26
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-05 13:20:01 +1200

    add class SportsCarTS

---
+++ ObjectContextApp/SportsCarTS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObjectContextApp
+{
+    [Synchronization]
+    class SportsCarTS : ContextBoundObject
+    {
+        public SportsCarTS()
+        {
+            // Get context information and print out context ID.
+            Context ctx = Thread.CurrentContext;
+            Console.WriteLine("{0} object in context{ 1} ", this.ToString(), ctx.ContextID);
+            foreach (IContextProperty itfCtxProp in ctx.ContextProperties)
+                Console.WriteLine("-> Ctx Prop: {0}", itfCtxProp.Name);
+        }
+    }
+}
