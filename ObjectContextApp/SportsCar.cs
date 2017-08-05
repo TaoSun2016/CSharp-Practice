f2bce2cdd82e9908e1698a507d4c787fec71c7f1
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-05 13:17:58 +1200

    add class SportsCar

---
+++ ObjectContextApp/SportsCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+
+namespace ObjectContextApp
+{
+    class SportsCar
+    {
+        public SportsCar()
+        {
+            // Get context information and print out context ID.
+            Context ctx = Thread.CurrentContext;
+            Console.WriteLine("{0} object in context{1}",this.ToString(), ctx.ContextID);
+            foreach (IContextProperty itfCtxProp in ctx.ContextProperties)
+                Console.WriteLine("-> Ctx Prop: {0}", itfCtxProp.Name);
+        }
+    }
+}
