1b317f304ac253769394ff40b39d94a911fb0a26
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-05 17:01:12 +1200

    modify method

--- SyncDelegateReview/Program.cs
+++ SyncDelegateReview/Program.cs
@@ -17,11 +17,21 @@ namespace SyncDelegateReview
             Console.WriteLine("Main() invoked on thread {0}.",Thread.CurrentThread.ManagedThreadId);
             // Invoke Add() in a synchronous manner.
             BinaryOp b = new BinaryOp(Add);
+            IAsyncResult asresult = b.BeginInvoke(10, 10, null, null);
             // Could also write b.Invoke(10, 10);
-            int answer = b(10, 10);
+            //int answer = b(10, 10);
             // These lines will not execute until
             // the Add() method has completed.
-            Console.WriteLine("Doing more work in  Main()!");
+            /*while (!asresult.IsCompleted)
+            {
+                Console.WriteLine("Doing more work in  Main()!");
+                Thread.Sleep(1000);
+            }*/
+            while (!asresult.AsyncWaitHandle.WaitOne(1000,true))
+            {
+                Console.WriteLine("Doing more work in Main()!");
+            }
+            int answer = b.EndInvoke(asresult);
             Console.WriteLine("10 + 10 is {0}.", answer);
             Console.ReadLine();
         }
