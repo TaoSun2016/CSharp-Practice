0badae8218b9825ae3a695f2669133ca3a2a094e
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-06 13:28:18 +1200

    implement methods

--- AsyncCallbackDelegate/Program.cs
+++ AsyncCallbackDelegate/Program.cs
@@ -2,14 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncCallbackDelegate
 {
+    public delegate int BinaryOp(int x, int y);
     class Program
     {
+
+        private static bool isDone = false;
         static void Main(string[] args)
         {
+            Console.WriteLine("*****AsyncCallbackDelegate Example * ****");
+            Console.WriteLine("Main() invoked on thread { 0}.", Thread.CurrentThread.ManagedThreadId);
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+            // Assume other work is performed here...
+            while (!isDone)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Working....");
+            }
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            Console.WriteLine("Add() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+        static void AddComplete(IAsyncResult itfAR)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Your addition is complete");
+            isDone = true;
         }
     }
 }
