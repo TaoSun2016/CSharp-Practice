292764008c8514e3422fb423a692176253279dfd
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-07 11:01:32 +1200

    add project TimerApp

---
+++ TimerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void PrintTime(object state)
+        {
+            Console.WriteLine("Time is: {0}",
+            DateTime.Now.ToLongTimeString());
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
