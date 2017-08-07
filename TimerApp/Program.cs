147f663b1c8f2f997f9eb6071e33ad8852af3ebe
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-07 13:50:04 +1200

    add new project ThreadPoolApp

--- TimerApp/Program.cs
+++ TimerApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TimerApp
@@ -15,6 +16,16 @@ namespace TimerApp
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Working with Timer type * ****\n");
+            // Create the delegate for the Timer type.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+            // Establish timer settings.
+            Timer t = new Timer( timeCB, // The TimerCallback delegate object.
+            null, // Any info to pass into the called method(null for no info).
+            0, // Amount of time to wait before starting(in milliseconds).
+            1000); // Interval of time between calls(in milliseconds).
+            Console.WriteLine("Hit key to terminate...");
+            Console.ReadLine();
         }
     }
 }
