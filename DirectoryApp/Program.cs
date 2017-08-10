7733023b20309be5db2be70e76f8db1f79c1c71c
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-10 13:59:40 +1200

    add new project DirectoryApp

---
+++ DirectoryApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with  Directory(Info) * ****\n");
+            ShowWindowsDirectoryInfo();
+            Console.ReadLine();
+        }
+
+        static void ShowWindowsDirectoryInfo()
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\windows");
+            Console.WriteLine(di.FullName);
+            Console.WriteLine(di.Name);
+            Console.WriteLine(di.Root);
+            Console.WriteLine(di.Attributes);
+            Console.WriteLine(di.CreationTime);
+        }
+    }
+}
