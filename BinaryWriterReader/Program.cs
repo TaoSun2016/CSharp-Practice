4378b2901455d6f736dbfbcc91bf0582fabe72bd
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-12 11:05:21 +1200

    Add Write() mehtod

--- BinaryWriterReader/Program.cs
+++ BinaryWriterReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace BinaryWriterReader
     {
         static void Main(string[] args)
         {
+            Write();
+        }
+        static private void Write()
+        {
+            Console.WriteLine("***** Fun with Binary Writers / Readers * ****\n");
+            // Open a binary writer for a file.
+            FileInfo f = new FileInfo("BinFile.dat");
+            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
+            {
+                // Print out the type of BaseStream.
+                // (System.IO.FileStream in this case).
+                Console.WriteLine("Base stream is: {0}", bw.BaseStream);
+                // Create some data to save in the file.
+                double aDouble = 1234.67;
+                int anInt = 34567;
+                string aString = "A, B, C";
+                // Write the data.
+                bw.Write(aDouble);
+                bw.Write(anInt);
+                bw.Write(aString);
+            }
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
     }
 }
