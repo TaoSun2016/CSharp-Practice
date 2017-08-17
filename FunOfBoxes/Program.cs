af996c1a7d1ad97e680705633d4209f34a3866a2
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-18 11:05:50 +1200

    add method to project

--- FunOfBoxes/Program.cs
+++ FunOfBoxes/Program.cs
@@ -12,8 +12,58 @@ namespace FunOfBoxes
        
         public static void Main(string[] args)
         {
+            int tryTimes = 0;
+            int oldIndex = -1;
+            int newIndex = -1;
+            char oldValue = '0';
+            char newValue = '0';
             Boxes boxes = new Boxes();
             boxes.ShowBoxes();
+            do
+            {
+                Console.Clear();
+                boxes.ShowBoxes();
+                
+                Console.WriteLine("Please input a valid box number:");
+
+                newIndex = int.Parse(Console.ReadLine());
+                Console.WriteLine($"input[{newIndex}]");
+                newValue = boxes.GetValue(newIndex);
+                Console.WriteLine($"value={newValue}");
+
+
+                if (newValue == '0')
+                {
+                    continue;
+                }
+
+                tryTimes++;
+
+                if (oldIndex!=-1)
+                {
+                    oldValue = boxes.GetValue(oldIndex);
+                }
+                else
+                {
+                    oldValue = '0';
+                }
+                
+                if (oldValue == newValue)
+                {
+                    boxes.SetValue(oldIndex,'0');
+                    boxes.SetValue(newIndex, '0');
+                    if (boxes.GetCount() == 0)
+                    {
+                        Console.WriteLine("Congratulation! You win!!!");
+                        return;
+                    }
+                    oldIndex = -1;
+                }
+                else
+                {
+                    oldIndex = newIndex;
+                }
+            } while (tryTimes < MaxTry);
         }
 
    
@@ -44,9 +94,31 @@ namespace FunOfBoxes
             {
                 if (boxes[i]!='0')
                 {
-                    Console.WriteLine("["+i+"] ");
+                    Console.Write("["+i+":"+boxes[i]+"] ");
                 }
             }
+            Console.WriteLine();
+        }
+
+        public char GetValue(int index)
+        {
+            return boxes[index];
+        }
+        public void SetValue(int index, char value)
+        {
+            boxes[index] = value;
+        }
+        public int GetCount()
+        {
+            int count = 0;
+            for (int i=0;i<boxes.Length;i++)
+            {
+                if (boxes[i]!='0')
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
