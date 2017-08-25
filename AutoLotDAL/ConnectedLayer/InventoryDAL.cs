4820b3cce1086ca8cd745384320c2362cc317197
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-25 18:50:01 +1200

    add some methods

---
+++ AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -0,0 +1,59 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using AutoLotDAL.Models;
+using System;
+
+namespace AutoLotDAL.ConnectedLayer
+{
+    class InventoryDAL
+    {
+        private SqlConnection _sqlConnection = null;
+        public void OpenConnection(string connectionString)
+        {
+            _sqlConnection = new SqlConnection{ ConnectionString = connectionString };
+            _sqlConnection.Open();
+        }
+        public void CloseConnection()
+        {
+            _sqlConnection.Close();
+        }
+        public void InsertAuto(int id, string color,string make, string petName)
+        {
+            // Format and execute SQL statement.
+            string sql = "Insert Into Inventory" + $"(Make, Color, PetName) Values ('{make}','{color}', '{petName}')";
+            // Execute using our connection.
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        public void InsertAuto(NewCar car)
+        {
+            // Format and execute SQL statement.
+            string sql = "Insert Into Inventory" + "( Make, Color, PetName) Values" +  $"('{car.Make}', '{car.Color}','{car.PetName}')";
+            // Execute using our connection.
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        public void DeleteCar(int id)
+        {
+            // Delete the car with the specified CarId
+            string sql = $"Delete from Inventory where CarId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql,_sqlConnection))
+            {
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry!  That car is on order!", ex);
+                    throw error;
+                }
+            }
+        }
+    }
+}
