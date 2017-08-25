37559d5758d7c3087c3325f2de3d48aa64fb6416
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-25 17:17:56 +1200

    output DbConnection's property

--- AutoLotDataReader/Program.cs
+++ AutoLotDataReader/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AutoLotDataReader
 {
@@ -10,6 +13,37 @@ namespace AutoLotDataReader
     {
         static void Main(string[] args)
         {
+            WriteLine("***** Fun with Data Readers * ****\n");
+            // Create and open a connection.
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source=DESKTOP-PRHUBC6;Integrated Security=SSPI;" +
+                "Initial Catalog=AutoLot;Connect Timeout=30";
+                connection.Open();
+                ShowConnectionStatus(connection);
+                // Create a SQL command object.
+                string sql = "Select * From Inventory";
+                SqlCommand myCommand = new SqlCommand(sql, connection);
+                // Obtain a data reader a  ExecuteReader().
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    // Loop over the results.
+                    while (myDataReader.Read())
+                    {
+                        WriteLine($"-> Make:{myDataReader["Make"]}, PetName:{ myDataReader["PetName"]}, Color:{ myDataReader["Color"]}.");
+                    }
+                }
+            }
+            ReadLine();
+        }
+        static void ShowConnectionStatus(SqlConnection connection)
+        {
+            // Show various stats about current connection object.
+            WriteLine("***** Info about your connection * ****");
+            WriteLine($"Database location:{connection.DataSource}");
+            WriteLine($"Database name:{connection.Database}");
+            WriteLine($"Timeout:{connection.ConnectionTimeout}");
+            WriteLine($"Connection state:{connection.State}\n");
         }
     }
 }
