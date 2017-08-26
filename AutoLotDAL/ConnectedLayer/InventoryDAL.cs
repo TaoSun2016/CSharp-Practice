604eb83fcb73cbbbb75a3c9d914c3ee4b0d88eef
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-26 13:04:53 +1200

    add LookUpPetName()

--- AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -130,5 +130,40 @@ namespace AutoLotDAL.ConnectedLayer
             }
             return dataTable;
         }
+        public string LookUpPetName(int carID)
+        {
+            string carPetName;
+            // Establish name of stored proc.
+            using (SqlCommand command = new SqlCommand("GetPetName", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                // Input param.
+                SqlParameter param = new SqlParameter
+                {
+                    ParameterName = "@carID",
+                    SqlDbType = SqlDbType.Int,
+                    Value = carID,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(param);
+
+                // Output param.
+                param = new SqlParameter
+                {
+                    ParameterName = "@petName",
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10,
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(param);
+
+                // Execute the stored proc.
+                command.ExecuteNonQuery();
+                // Return output param.
+                carPetName = (string)command.Parameters["@petName"].Value;
+            }
+            return carPetName;
+        }
     }
+
 }
