3f7849627dc143e0599bddfe20e2ab82c1ad9b87
TaoSun2016 <kevinsun9516@gmail.com>
2017-08-26 13:15:22 +1200

    add new project

---
+++ AutoLotCUIClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using AutoLotDAL.ConnectedLayer;
+using AutoLotDAL.Models;
+using System.Configuration;
+using System.Data;
+using static System.Console;
+
+namespace AutoLotCUIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
