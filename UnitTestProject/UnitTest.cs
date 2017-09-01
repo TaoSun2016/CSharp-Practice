fd5223a69d75bb9c37c4aeac544c18064053f59e
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-01 12:22:55 +1200

    add test code

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            //Arrange
+            int num1 = 3;
+            int num2 = 4;
+            //Action
+            int result = MyMath.Add(num1, num2);
+            //Assert
+            Assert.AreEqual(result,num1+num2);
+        }
+    }
+}
