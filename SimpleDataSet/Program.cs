fb844b0a2e6c4167ef260f0a428daf5d82791bef
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-02 10:35:07 +1200

    add new metod PrintDataSet

--- SimpleDataSet/Program.cs
+++ SimpleDataSet/Program.cs
@@ -26,11 +26,51 @@ namespace SimpleDataSet
         }
 
         static void FillDataSet(DataSet ds) {
-            var carIDColumn = new DataColumn("CarID", typeof(int)) { Caption = "Car ID", ReadOnly = true, AllowDBNull = false, Unique = true };
+            var carIDColumn = new DataColumn("CarID", typeof(int))
+            { Caption = "Car ID", ReadOnly = true, AllowDBNull = false, Unique = true ,
+                AutoIncrement = true,
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1
+            };
             var carMakeColumn = new DataColumn("Make",typeof(string));
+            var carColorColumn = new DataColumn("Color",typeof(string));
             var carPetNameColumn = new DataColumn("PetName", typeof(string)) { Caption="Pet Name"};
+
+            var inventoryTable = new DataTable("Inventory");
+            inventoryTable.Columns.AddRange(new[]
+            {carIDColumn, carMakeColumn, carColorColumn, carPetNameColumn});
         }
 
         static void PrintDataSet(DataSet ds) { }
+        private static void ManipulateDataRowState()
+        {
+            // Create a temp DataTable for testing.
+            var temp = new DataTable("Temp");
+            temp.Columns.Add(new DataColumn("TempColumn", typeof(int)));
+
+            // RowState = Detached.
+            var row = temp.NewRow();
+            WriteLine($"After calling NewRow():{row.RowState}");
+
+            // RowState = Added.
+            temp.Rows.Add(row);
+            WriteLine($"After calling Rows.Add():{row.RowState}");
+
+            // RowState = Added.
+            row["TempColumn"] = 10;
+            WriteLine($"After first assignment:{row.RowState}");
+
+            // RowState = Unchanged.
+            temp.AcceptChanges();
+            WriteLine($"After calling AcceptChanges:{row.RowState}");
+
+            // RowState = Modified.
+            row["TempColumn"] = 11;
+            WriteLine($"After first assignment:{row.RowState}");
+
+            // RowState = Deleted.
+               temp.Rows[0].Delete();
+            WriteLine($"After calling Delete:{row.RowState}");
+        }
     }
 }
