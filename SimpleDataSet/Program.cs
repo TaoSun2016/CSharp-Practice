34e3d2535b1478b33232a478dcfaaf688e1b11c8
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-01 14:14:27 +1200

    add method

--- SimpleDataSet/Program.cs
+++ SimpleDataSet/Program.cs
@@ -14,15 +14,23 @@ namespace SimpleDataSet
         {
             WriteLine("***** Fun with DataSets *****\n");
             // Create the DataSet object and add a few  properties.
-            var carInventoryDS = new DataSet("car inventory");
+            var carsInventoryDS = new DataSet("car inventory");
 
-            carInventoryDS.ExtendedProperties["TimeStamp"] = DateTime.Now;
-            carInventoryDS.ExtendedProperties["DataSetID"] = Guid.NewGuid();
-            carInventoryDS.ExtendedProperties["Company"] = "Mikko’s Hot Tub Super Store";
+            carsInventoryDS.ExtendedProperties["TimeStamp"] = DateTime.Now;
+            carsInventoryDS.ExtendedProperties["DataSetID"] = Guid.NewGuid();
+            carsInventoryDS.ExtendedProperties["Company"] = "Mikko’s Hot Tub Super Store";
 
             FillDataSet(carsInventoryDS);
             PrintDataSet(carsInventoryDS);
             ReadLine();
         }
+
+        static void FillDataSet(DataSet ds) {
+            var carIDColumn = new DataColumn("CarID", typeof(int)) { Caption = "Car ID", ReadOnly = true, AllowDBNull = false, Unique = true };
+            var carMakeColumn = new DataColumn("Make",typeof(string));
+            var carPetNameColumn = new DataColumn("PetName", typeof(string)) { Caption="Pet Name"};
+        }
+
+        static void PrintDataSet(DataSet ds) { }
     }
 }
