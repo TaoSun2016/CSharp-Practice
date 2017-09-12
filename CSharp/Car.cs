ef215a072f25c6ec9a8c453f8c552a0bb3091193
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-12 19:57:53 +1200

    add class Car

---
+++ CSharp/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Car
+    {
+        public string RegisterNumber { get; set; }
+        public int Seats { get; set; }
+        public int Windows { get; set; }
+        public int Wheels { get; set; }
+    }
+}
