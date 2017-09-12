5c28032bb615025489041dd2411f8685dbea4be7
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-12 20:02:03 +1200

    add class HondaCar

---
+++ CSharp/HondaCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class HondaCar:Car
+    {
+        public string GetRegisterNumber()
+        {
+            return RegisterNumber;
+        }
+
+        public int GetWindowsNumber()
+        {
+            return Windows;
+        }
+        public int GetWheelsNumber()
+        {
+            return Wheels;
+        }
+        public int GetSeatsNumber()
+        {
+            return Seats;
+        }
+    }
+}
