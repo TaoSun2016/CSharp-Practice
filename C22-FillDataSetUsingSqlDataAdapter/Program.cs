79a4c2dfc8714fff93ede2eb2b0d4343f28ed7cc
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-16 20:17:01 +1200

    add connectionString

--- C22-FillDataSetUsingSqlDataAdapter/Program.cs
+++ C22-FillDataSetUsingSqlDataAdapter/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Collections;
+using System.Data.SqlClient;
+
 
 namespace C22_FillDataSetUsingSqlDataAdapter
 {
@@ -10,6 +14,8 @@ namespace C22_FillDataSetUsingSqlDataAdapter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Data Adapters * ****\n");
+            string connectionString = "Data Source=DESKTOP-PRHUBC6,Integrated Security = SSPI, Initial Catalog=AutoLot";
         }
     }
 }
