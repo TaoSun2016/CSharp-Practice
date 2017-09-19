2d2de63d41f70231ee112015dcb89e361d303c85
TaoSun2016 <kevinsun9516@gmail.com>
2017-09-19 13:30:46 +1200

    test array

--- TestForWork/Program.cs
+++ TestForWork/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TestForWork
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[,] intMyArr = { { 7, 1, 3 }, { 2, 9, 6 } };
+            Console.WriteLine(intMyArr.GetUpperBound(1));
+            Console.WriteLine(intMyArr.Length);
+            Console.WriteLine(intMyArr.GetUpperBound(0));
+
         }
     }
 }
