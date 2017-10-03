7be6d5223f5a2d5d47869f49037bc41dc369c93d
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-03 19:18:08 +1300

    add class Operand

--- IC-MVCTEST/Models/Operand.cs
+++ IC-MVCTEST/Models/Operand.cs
@@ -7,5 +7,9 @@ namespace IC_MVCTEST.Models
 {
     public class Operand
     {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public string Message { get; set; }
     }
 }
